ce9da13c27ce65924aaa07fc819f38b3ec20e805
Heather Wenzel <heather.wenzel@ge.com>
2022-01-18 08:44:30 -0500

    Set up local DB

--- src/main/java/com/heatherwenzel/springdatademo/bootstrap/DataInitializer.java
+++ src/main/java/com/heatherwenzel/springdatademo/bootstrap/DataInitializer.java
@@ -3,8 +3,10 @@ package com.heatherwenzel.springdatademo.bootstrap;
 import com.heatherwenzel.springdatademo.domain.Book;
 import com.heatherwenzel.springdatademo.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({"local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -15,6 +17,8 @@ public class DataInitializer implements CommandLineRunner {
     }
 
     public void run(String... args) throws Exception {
+
+        bookRepository.deleteAll();
 
         Book bookDDD = new Book("Domain Driven Design", "123", "Random House");
         System.out.println("ID: " + bookDDD.getId());
